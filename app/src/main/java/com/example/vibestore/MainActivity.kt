5ca72831dacca2ae647cf78509594721c6f77f0a
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-04-07 07:00:36 +0700

    Add dependencies icon extended and create search bar

--- app/src/main/java/com/example/vibestore/MainActivity.kt
+++ app/src/main/java/com/example/vibestore/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.vibestore
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.example.vibestore.ui.component.Search
 import com.example.vibestore.ui.theme.VibeStoreTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -18,6 +22,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    Scaffold(
+                        topBar = {
+                            Search()
+                        }
+                    ) {
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/example/vibestore/ui/component/Search.kt
@@ -0,0 +1,104 @@
+package com.example.vibestore.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.graphics.toColorInt
+import com.example.vibestore.ui.theme.VibeStoreTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Search() {
+    Row(
+        verticalAlignment = Alignment.Bottom
+    ) {
+        SearchBar(
+            query = "",
+            onQueryChange = {},
+            onSearch = {},
+            colors = SearchBarDefaults.colors(
+                containerColor = Color("#DDDDDD".toColorInt())
+            ),
+            active = false,
+            onActiveChange = {},
+            modifier = Modifier
+                .padding(16.dp)
+                .weight(2f)
+                .fillMaxWidth(),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search Icon",
+                    tint = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            },
+            placeholder = {
+                Text(
+                    text = "search product...",
+                    color = MaterialTheme.colorScheme.outline
+                )
+            }
+        ) {
+        }
+        Box(
+            modifier = Modifier
+                .padding(
+                    start = 0.dp,
+                    end = 16.dp,
+                    top = 16.dp,
+                    bottom = 16.dp
+                )
+                .size(56.dp)
+                .background(
+                    color = Color("#DDDDDD".toColorInt()),
+                    shape = RoundedCornerShape(50.dp)
+                )
+                .padding(14.dp),
+        ){
+            Icon(
+                imageVector = Icons.Outlined.Notifications,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .size(24.dp),
+                tint = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SearchPreview() {
+    VibeStoreTheme {
+        Search()
+    }
+}
