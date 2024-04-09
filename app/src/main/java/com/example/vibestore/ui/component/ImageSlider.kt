7693e108ffe32eba5bd4cee1aa730ea27b04c2f4
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-04-09 09:17:48 +0700

    Add logic auto image slider

--- app/src/main/java/com/example/vibestore/ui/component/ImageSlider.kt
+++ app/src/main/java/com/example/vibestore/ui/component/ImageSlider.kt
@@ -1,5 +1,6 @@
 package com.example.vibestore.ui.component
 
+import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -18,6 +19,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -28,6 +30,7 @@ import androidx.compose.ui.unit.dp
 import androidx.core.graphics.toColorInt
 import com.example.vibestore.model.dummySliderImage
 import com.example.vibestore.ui.theme.VibeStoreTheme
+import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -82,6 +85,16 @@ fun ImageSlider() {
                         .size(8.dp)
                 )
             }
+        }
+    }
+
+    // Auto Slide
+    LaunchedEffect(Unit) {
+        while(true) {
+            delay(4000)
+            Log.d("Tag", "${pagerState.currentPage}")
+            val nextPage = (pagerState.currentPage + 1) % pagerState.pageCount
+            pagerState.animateScrollToPage(nextPage)
         }
     }
 }
