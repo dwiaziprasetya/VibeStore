775e3ef0d9b9251876a18b8405bfcb21273bff1c
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-04-16 19:11:25 +0700

    Modify Search component

--- app/src/main/java/com/example/vibestore/ui/component/Search.kt
+++ app/src/main/java/com/example/vibestore/ui/component/Search.kt
@@ -1,13 +1,7 @@
 package com.example.vibestore.ui.component
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -17,80 +11,48 @@ import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.graphics.toColorInt
-import com.example.vibestore.R
 import com.example.vibestore.ui.theme.VibeStoreTheme
 import com.example.vibestore.ui.theme.poppinsFontFamily
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Search() {
-    Row(
-        verticalAlignment = Alignment.Bottom,
-    ) {
-        SearchBar(
-            query = "",
-            onQueryChange = {},
-            onSearch = {},
-            colors = SearchBarDefaults.colors(
-                containerColor = Color("#EEEEEE".toColorInt())
-            ),
-            active = false,
-            onActiveChange = {},
-            modifier = Modifier
-                .padding(16.dp)
-                .weight(2f)
-                .fillMaxWidth(),
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Search,
-                    contentDescription = "Search Icon",
-                    tint = MaterialTheme.colorScheme.outline
-                )
-            },
-            placeholder = {
-                Text(
-                    text = "Search product...",
-                    color = MaterialTheme.colorScheme.outline,
-                    fontSize = 16.sp,
-                    fontFamily = poppinsFontFamily,
-                    fontWeight = FontWeight.Normal,
-                )
-            }
-        ) {
-        }
-        Box(
-            modifier = Modifier
-                .padding(
-                    start = 0.dp,
-                    end = 16.dp,
-                    top = 16.dp,
-                    bottom = 16.dp
-                )
-                .size(56.dp)
-                .background(
-                    color = Color("#EEEEEE".toColorInt()),
-                    shape = RoundedCornerShape(50.dp)
-                )
-                .padding(16.dp),
-        ){
+    SearchBar(
+        query = "",
+        onQueryChange = {},
+        onSearch = {},
+        colors = SearchBarDefaults.colors(
+            containerColor = Color("#EEEEEE".toColorInt())
+        ),
+        active = false,
+        onActiveChange = {},
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        leadingIcon = {
             Icon(
-                painter = painterResource(R.drawable.notificationoutlined),
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .size(24.dp),
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search Icon",
                 tint = MaterialTheme.colorScheme.outline
             )
+        },
+        placeholder = {
+            Text(
+                text = "Search product...",
+                color = MaterialTheme.colorScheme.outline,
+                fontSize = 16.sp,
+                fontFamily = poppinsFontFamily,
+                fontWeight = FontWeight.Normal,
+            )
         }
+    ) {
     }
 }
 
