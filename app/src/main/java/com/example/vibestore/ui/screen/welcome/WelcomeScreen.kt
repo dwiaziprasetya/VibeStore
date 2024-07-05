04649026d4a05468e05d4a817870e8768050622b
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-07-05 15:52:10 +0700

    Add bottom sheet registration

--- app/src/main/java/com/example/vibestore/ui/screen/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/example/vibestore/ui/screen/welcome/WelcomeScreen.kt
@@ -1,26 +1,44 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.vibestore.ui.screen.welcome
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -37,18 +55,32 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.vibestore.R
 import com.example.vibestore.model.dummyTextWelcome
-import com.example.vibestore.ui.navigation.Screen
 import com.example.vibestore.ui.theme.VibeStoreTheme
 import com.example.vibestore.ui.theme.poppinsFontFamily
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun WelcomeScreen(navController: NavHostController) {
+    val systemUiController = rememberSystemUiController()
+
     val pagerState = rememberPagerState(
         pageCount = {
             dummyTextWelcome.size
         }
     )
+    var isSheetOpen by rememberSaveable { mutableStateOf(false) }
+    val modalBottomSheetState = rememberModalBottomSheetState()
+
+    if (isSheetOpen) {
+        BottomSheetRegister(
+            onDismiss = {
+                isSheetOpen = false
+            },
+            sheetState = modalBottomSheetState,
+        )
+    }
+
     Box {
         Image(
             painter = painterResource(R.drawable.aaaaaa),
@@ -118,7 +150,7 @@ fun WelcomeScreen(navController: NavHostController) {
                         .fillMaxWidth(),
                     shape = RoundedCornerShape(40.dp),
                     onClick = {
-                        navController.navigate(Screen.Home.route)
+                        isSheetOpen = true
                     },
                     colors = ButtonDefaults.buttonColors(
                         containerColor = Color.Black
@@ -133,6 +165,100 @@ fun WelcomeScreen(navController: NavHostController) {
                 }
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheetRegister(
+    onDismiss: () -> Unit,
+    sheetState: SheetState
+) {
+    val bottomPadding = WindowInsets.navigationBars.asPaddingValues()
+        .calculateBottomPadding().value.toInt() + 8
+    ModalBottomSheet(
+        onDismissRequest = onDismiss,
+        sheetState = sheetState,
+        modifier = Modifier.statusBarsPadding(),
+        windowInsets = BottomSheetDefaults
+            .windowInsets
+            .only(
+                WindowInsetsSides.Bottom
+            )
+    ) {
+        BottomSheetWelcomeContent()
+    }
+}
+
+@Composable
+fun BottomSheetWelcomeContent(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .height(288.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .background(Color.White)
+                .align(Alignment.Center)
+        ) {
+            Button(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .height(55.dp)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(40.dp),
+                onClick = {
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color("#29bf12".toColorInt())
+                )
+            ) {
+                Text(
+                    fontFamily = poppinsFontFamily,
+                    text = "Sign Up",
+                    fontSize = 16.sp,
+                    modifier = Modifier
+                )
+            }
+            Button(
+                modifier = Modifier
+                    .padding(
+                        start = 16.dp,
+                        top = 16.dp,
+                        end = 16.dp
+                    )
+                    .height(55.dp)
+                    .fillMaxWidth()
+                    .border(
+                        color = Color.Black,
+                        width = 0.1.dp,
+                        shape = RoundedCornerShape(40.dp)
+                    ),
+                shape = RoundedCornerShape(40.dp),
+                onClick = {
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White
+                )
+            ) {
+                Text(
+                    fontFamily = poppinsFontFamily,
+                    text = "Login to Vibe Store",
+                    fontSize = 16.sp,
+                    color = Color.Black,
+                    modifier = Modifier
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BottomSheetRegisterPreview() {
+    VibeStoreTheme {
+        BottomSheetWelcomeContent()
     }
 }
 
