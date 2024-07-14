4861497bde6f31f5557b4f22c4a64b8b8cb59fd1
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-07-14 10:52:02 +0700

    Add AuthNav and MainNav route

--- app/src/main/java/com/example/vibestore/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/vibestore/ui/navigation/Screen.kt
@@ -1,17 +1,24 @@
 package com.example.vibestore.ui.navigation
 
 sealed class Screen(val route: String) {
-    object Welcome : Screen("welcome")
-    object SignUp : Screen("signup")
-    object Login : Screen("login")
-    object Home : Screen("home")
-    object Categories : Screen("categories")
-    object Coupon : Screen("coupon")
-    object Favourite : Screen("favourite")
-    object Profile : Screen("profile")
-    object OurProduct : Screen("myproduct")
-    object MyCart : Screen("mycart")
-    object DetailProduct : Screen("home/{productId}") {
+
+    // Screen Routes
+    data object Welcome : Screen("welcome")
+    data object SignUp : Screen("signup")
+    data object Login : Screen("login")
+    data object Home : Screen("home")
+    data object Categories : Screen("categories")
+    data object Coupon : Screen("coupon")
+    data object Favourite : Screen("favourite")
+    data object Profile : Screen("profile")
+    data object OurProduct : Screen("myproduct")
+    data object MyCart : Screen("mycart")
+    data object DetailProduct : Screen("home/{productId}") {
         fun createRoute(productId: Int) = "home/$productId"
     }
+
+    // Graph Routes
+    data object AuthNav : Screen("AUTH_NAV_GRAPH")
+    data object MainNav : Screen("MAIN_NAV_GRAPH")
+
 }
